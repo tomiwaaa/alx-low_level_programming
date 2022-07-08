67d47d184e6082d6ba2d24dab34bf005007dfb80
tomiwaaa <dosunmutomiwdosunmutomiwa29@gmail.com>
2022-07-08 07:49:43 -0700

    removing infinite loop

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,15 +1,16 @@
-i
 #include <stdio.h>
 
 
 
 /**
- *
- * * main - causes an infinite loop
- *
- * * Return: 0
- *
- * */
+
+* main - causes an infinite loop
+
+*
+
+* Return: 0
+
+*/
 
 
 
@@ -17,33 +18,32 @@ int main(void)
 
 {
 
-	        int i;
+        int i;
 
 
 
-		        printf("Infinite loop incoming :(\n");
+        printf("Infinite loop incoming :(\n");
 
 
 
-			        i = 0;
+        i = 0;
 
-					/**
-					 *
-					 *         *while (i < 10)
-					 *
-					 *                 *{
-					 *
-					 *                         *       putchar(i);
-					 *
-					 *                                 *}
-					 *
-					 *                                 	*/
+	/*
 
-				        printf("Infinite loop avoided! \\o/\n");
+        *while (i < 10)
+
+        *{
+
+        *       putchar(i);
+
+        *}
+
+	*/
+
+        printf("Infinite loop avoided! \\o/\n");
 
 
 
-					        return (0);
+        return (0);
 
 }
-
