d3f9a808b7b91302ecda5d4eb3c52dae89eea83b
tomiwaaa <dosunmutomiwdosunmutomiwa29@gmail.com>
2022-07-13 08:20:34 -0700

    task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,19 +1,11 @@
 /**
-
  * swap_int – swaps the value of two integers 
-
  *using two input parameters
-
  *
-
  *@a: input parameter 1
-
  *@b: input parameter 2 
-
  *
-
  *Return: Nothing
-
  */
 
 void swap_int(int *a, int *b)
@@ -21,13 +13,9 @@ void swap_int(int *a, int *b)
 {
   
   int temp;
-  
-  temp = *a;
-  
-  *a = *b;
-  
-  *b = temp;
-  
+   temp = *a;
+   *a = *b;
+   *b = temp;
 }
 
 
